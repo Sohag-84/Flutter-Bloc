2e985bdf102c73df3223493135f969948562c53b
Sohag-84 <ihsohag84@gmail.com>
2024-01-13 19:22:56 +0600

    rebuild only counter text widget

--- counter_app/lib/home_page.dart
+++ counter_app/lib/home_page.dart
@@ -16,34 +16,34 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder(
-        bloc: counterCubit,
-        builder: (context, counter) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              title: Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
+            BlocBuilder<CounterCubit, int>(
+                bloc: counterCubit,
+                builder: (context, counter) {
+                  return Text(
                     '$counter',
                     style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                ],
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () => counterCubit.increment(),
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
-          );
-        });
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counterCubit.increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
