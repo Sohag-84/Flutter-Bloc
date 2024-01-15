c304e64b2388782035ad1e4ac791242b77ea5acf
Sohag-84 <ihsohag84@gmail.com>
2024-01-15 19:26:21 +0600

    login page is designed

---
+++ form_validation/lib/login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/gradient_button.dart';
+import 'widgets/login_field.dart';
+import 'widgets/social_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/images/signin_balls.png'),
+              const Text(
+                'Sign in.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+              const SizedBox(height: 50),
+              const SocialButton(
+                iconPath: 'assets/svgs/g_logo.svg',
+                label: 'Continue with Google',
+              ),
+              const SizedBox(height: 20),
+              const SocialButton(
+                iconPath: 'assets/svgs/f_logo.svg',
+                label: 'Continue with Facebook',
+                horizontalPadding: 90,
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                'or',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              const SizedBox(height: 15),
+              LoginField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
+              const SizedBox(height: 15),
+              LoginField(
+                hintText: 'Password',
+                controller: passwordController,
+              ),
+              const SizedBox(height: 20),
+              const GradientButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- form_validation/lib/widgets/social_button.dart
+++ form_validation/lib/widgets/social_button.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:form_validation/pallet.dart';
@@ -20,6 +22,7 @@ class SocialButton extends StatelessWidget {
       icon: SvgPicture.asset(
         iconPath,
         width: 25,
+        color: Pallete.whiteColor,
       ),
       label: Text(
         label,
