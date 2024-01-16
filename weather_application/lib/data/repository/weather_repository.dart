94b2a4f186f461091d8300da99334b1983702c5d
Sohag-84 <ihsohag84@gmail.com>
2024-01-16 15:55:55 +0600

    create model class repository class

---
+++ weather_application/lib/data/repository/weather_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:weather_application/data/data_provider/weather_data_provider.dart';
+import 'package:weather_application/models/weather_model.dart';
+
+class WeatherRepository {
+  final WeatherDataProvider weatherDataProvider;
+  WeatherRepository(this.weatherDataProvider);
+
+  Future<WeatherModel> getCurrentWeather() async {
+    try {
+      String cityName = 'London';
+      final weatherData =
+          await weatherDataProvider.getCurrentWeather(cityName: cityName);
+
+      final data = jsonDecode(weatherData);
+
+      if (data['cod'] != '200') {
+        throw 'An unexpected error occurred';
+      }
+
+      return WeatherModel.fromMap(data);
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
