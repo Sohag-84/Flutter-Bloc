79d72d45fcf8218f68a89d283efde5a26a3b64b5
Sohag-84 <ihsohag84@gmail.com>
2024-01-21 17:42:06 +0600

    create counter event class

---
+++ bloc_counter_app/lib/bloc/counter_event.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class CounterEvent extends Equatable {
+  const CounterEvent();
+  @override
+  List<Object> get props => [];
+}
+
+class IncrementCounter extends CounterEvent{}
+
+class DecrementCounter extends CounterEvent{}
