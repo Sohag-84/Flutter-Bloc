7db753a67d70327159921498f6817108d6e32ab9
Sohag-84 <ihsohag84@gmail.com>
2024-01-21 19:16:30 +0600

    counter app complate using equitable package

--- bloc_counter_app/lib/main.dart
+++ bloc_counter_app/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_counter_app/bloc/counter_bloc.dart';
 import 'package:bloc_counter_app/ui/counter_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Counter App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: false,
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Counter App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: false,
+        ),
+        home: const CounterScreen(),
       ),
-      home: const CounterScreen(),
     );
   }
 }
--- bloc_counter_app/lib/ui/counter_screen.dart
+++ bloc_counter_app/lib/ui/counter_screen.dart
@@ -1,4 +1,9 @@
+import 'package:bloc_counter_app/bloc/counter_event.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/counter_bloc.dart';
+import '../bloc/counter_state.dart';
 
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
@@ -19,19 +24,34 @@ class _CounterScreenState extends State<CounterScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const Center(
-            child: Text(
-              "0",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
-            ),
+          BlocBuilder<CounterBloc, CounterState>(
+            builder: (context, state) {
+              return Center(
+                child: Text(
+                  state.counter.toString(),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 17),
+                ),
+              );
+            },
           ),
           const SizedBox(height: 15),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(onPressed: () {}, child: const Text("Increment")),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<CounterBloc>().add(IncrementCounter());
+                },
+                child: const Text("Increment"),
+              ),
               const SizedBox(width: 10),
-              ElevatedButton(onPressed: () {}, child: const Text("Decrement")),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<CounterBloc>().add(DecrementCounter());
+                },
+                child: const Text("Decrement"),
+              ),
             ],
           ),
         ],
