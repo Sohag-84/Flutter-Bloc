3c5f470b6d4afcccff4c4dcf4e6067d1c24b52c8
Sohag-84 <ihsohag84@gmail.com>
2024-01-23 10:43:09 +0600

    create utils file for pick gallery & camera image

--- image_picker/lib/utils/image_picker_utils.dart
+++ image_picker/lib/utils/image_picker_utils.dart
@@ -1,6 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImagePickerUtitls {
-  //final ImagePicker _picker = Image(image: ImagePicker());
+  final ImagePicker _picker = ImagePicker();
+
+  Future<XFile?> pickCameraImage() async {
+    XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    return image;
+  }
+
+  Future<XFile?> pickGalleryImage() async {
+    XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    return image;
+  }
 }
