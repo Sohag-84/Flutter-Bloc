e07eee9ce46b37ee17f7e23af8142f93a47fc938
Sohag-84 <ihsohag84@gmail.com>
2024-01-30 12:35:07 +0600

    remove unused code

--- e_commerce/lib/presentation/bottom_navbar/bloc/bottom_nav_bloc.dart
+++ e_commerce/lib/presentation/bottom_navbar/bloc/bottom_nav_bloc.dart
@@ -1,12 +1,11 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'bottom_nav_event.dart';
 part 'bottom_nav_state.dart';
 
 class BottomNavBloc extends Bloc<BottomNavEvent, BottomNavState> {
-  BottomNavBloc() : super(BottomNavInitial()) {
-    on<BottomNavEvent>((event, emit) {
-    });
+  BottomNavBloc() : super(BottomNavInitial(currentIndex: 0)) {
+    on<BottomNavEvent>((event, emit) {});
   }
 }
