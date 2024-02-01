f1e7d5f9eda08448b6e10f9ef034be80f1b9c90e
Sohag-84 <ihsohag84@gmail.com>
2024-02-01 20:52:03 +0600

    register bloc

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:e_commerce/presentation/bottom_navbar/bloc/bottom_nav_bloc.dart';
 import 'package:e_commerce/presentation/bottom_navbar/views/bottom_navbar_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
@@ -14,14 +16,21 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(398, 720),
       builder: (context, child) {
-        return MaterialApp(
-          title: 'Flutter E-commerce',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => BottomNavBloc(),
+            ),
+          ],
+          child: MaterialApp(
+            title: 'Flutter E-commerce',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const BottomNavbarView(),
           ),
-          home: const BottomNavbarView(),
         );
       },
     );
