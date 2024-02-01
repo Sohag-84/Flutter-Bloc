8e3979ba71c9dfe44bb2bfd7e35e9a793936c013
Sohag-84 <ihsohag84@gmail.com>
2024-02-01 21:15:51 +0600

    designed bottom navigation bar

--- e_commerce/lib/presentation/bottom_navbar/bloc/bottom_nav_state.dart
+++ e_commerce/lib/presentation/bottom_navbar/bloc/bottom_nav_state.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: must_be_immutable
+
 part of 'bottom_nav_bloc.dart';
 
 @immutable
 sealed class BottomNavState {}
 
 final class BottomNavInitialState extends BottomNavState {
-  final int currentIndex;
+  int currentIndex;
   BottomNavInitialState({required this.currentIndex});
 }
--- e_commerce/lib/presentation/bottom_navbar/views/bottom_navbar_view.dart
+++ e_commerce/lib/presentation/bottom_navbar/views/bottom_navbar_view.dart
@@ -1,7 +1,9 @@
+import 'package:e_commerce/pallet/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/bottom_nav_bloc.dart';
+import '../widgets/navbar_icon.dart';
 
 class BottomNavbarView extends StatefulWidget {
   const BottomNavbarView({super.key});
@@ -12,22 +14,98 @@ class BottomNavbarView extends StatefulWidget {
 
 class _BottomNavbarViewState extends State<BottomNavbarView> {
   final List pages = const [
-    Text("Home View"),
-    Text("Category View"),
-    Text("Search View"),
-    Text("Menu View"),
+    Center(child: Text("Home View")),
+    Center(child: Text("Category View")),
+    Center(child: Text("Search View")),
+    Center(child: Text("Menu View")),
   ];
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<BottomNavBloc, BottomNavState>(
       listener: (context, state) {},
       builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(),
-          bottomNavigationBar: BottomAppBar(
-            color: Colors.white,
-          ),
-        );
+        if (state is BottomNavInitialState) {
+          return Scaffold(
+            appBar: AppBar(),
+            bottomNavigationBar: BottomAppBar(
+              color: AppColors.whiteColor,
+              elevation: 1,
+              child: Row(
+                children: [
+                  //home
+                  Expanded(
+                    flex: 1,
+                    child: navbarIcon(
+                      onTap: () {
+                        context
+                            .read<BottomNavBloc>()
+                            .add(BottomNavPageIndexChangedEvent(pageIndex: 0));
+                      },
+                      icon: Icons.home_sharp,
+                      label: "Home",
+                      color: state.currentIndex == 0
+                          ? AppColors.pirmaryColor
+                          : AppColors.greyColor,
+                    ),
+                  ),
+                  //category
+                  Expanded(
+                    flex: 1,
+                    child: navbarIcon(
+                      onTap: () {
+                        context
+                            .read<BottomNavBloc>()
+                            .add(BottomNavPageIndexChangedEvent(pageIndex: 1));
+                      },
+                      icon: Icons.dashboard,
+                      label: "Category",
+                      color: state.currentIndex == 1
+                          ? AppColors.pirmaryColor
+                          : AppColors.greyColor,
+                    ),
+                  ),
+
+                  Expanded(flex: 1, child: Container()),
+                  //search
+                  Expanded(
+                    flex: 1,
+                    child: navbarIcon(
+                      onTap: () {
+                        context
+                            .read<BottomNavBloc>()
+                            .add(BottomNavPageIndexChangedEvent(pageIndex: 2));
+                      },
+                      icon: Icons.search,
+                      label: "Search",
+                      color: state.currentIndex == 2
+                          ? AppColors.pirmaryColor
+                          : AppColors.greyColor,
+                    ),
+                  ),
+                  //menu
+                  Expanded(
+                    flex: 1,
+                    child: navbarIcon(
+                      onTap: () {
+                        context
+                            .read<BottomNavBloc>()
+                            .add(BottomNavPageIndexChangedEvent(pageIndex: 3));
+                      },
+                      icon: Icons.menu,
+                      label: "Menu",
+                      color: state.currentIndex == 3
+                          ? AppColors.pirmaryColor
+                          : AppColors.greyColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            body: pages[state.currentIndex],
+          );
+        } else {
+          return const SizedBox();
+        }
       },
     );
   }
